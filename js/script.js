34cb8bad7c37385dd08b1fb887c450306d5ab25d
Ramaniuk <240724oi@gmail.com>
2021-09-13 16:37:42 -0400

    added rates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const numberOfFilms = +prompt("How many movies did you watch?", "");
 const personalMovieDB = {
     count : numberOfFilms,
@@ -7,8 +8,23 @@ const personalMovieDB = {
     privat : false
 };
 for (let i = 0; i < 2; i++) {
-    let lastMovie = prompt("What is your last whatched movie?", "");    
-    let rateMovie = prompt("What is your rate of last whatched movie?", "");
-    personalMovieDB.movies[lastMovie] = rateMovie;
+    let lastMovie = prompt("What is your last whatched movie?", ""),
+        rateMovie = prompt("What is your rate of last whatched movie?", "");
+    if (lastMovie === null || lastMovie === ""|| lastMovie.length > 50 || 
+        rateMovie === null || rateMovie === "" || rateMovie.length > 50) {
+            i--;
+    } else{ 
+        personalMovieDB.movies[lastMovie] = rateMovie;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("You've watch not many movies");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert("You have good rate");
+} else if (numberOfFilms > 30){
+    alert("You like movies!!!");
+} else {
+    alert("There is an error");
 }
 console.log(personalMovieDB);
