6c6942dbbcafce44b1786533f0ec9311dcf5cdc5
Ramaniuk <240724oi@gmail.com>
2021-09-15 12:22:16 -0400

    add functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 "use strict";
-const numberOfFilms = +prompt("How many movies did you watch?", "");
+let numberOfFilms;
+function start () {    
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+       numberOfFilms = +prompt("How many movies did you watch?", ""); 
+    } 
+}
+//start();
+ 
 const personalMovieDB = {
     count : numberOfFilms,
     movies : {},
@@ -7,7 +14,16 @@ const personalMovieDB = {
     genres : [],
     privat : false
 };
-for (let i = 0; i < 2; i++) {
+
+function showMyDB (hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } 
+}
+showMyDB(personalMovieDB["privat"]);
+
+function rememberMyFilms() {
+    for( let i = 0; i < 2; i++) {
     let lastMovie = prompt("What is your last whatched movie?", ""),
         rateMovie = prompt("What is your rate of last whatched movie?", "");
     if (lastMovie === null || lastMovie === ""|| lastMovie.length > 50 || 
@@ -16,15 +32,28 @@ for (let i = 0; i < 2; i++) {
     } else{ 
         personalMovieDB.movies[lastMovie] = rateMovie;
     }
+    }
 }
+//rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    alert("You've watch not many movies");
-} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-    alert("You have good rate");
-} else if (numberOfFilms > 30){
-    alert("You like movies!!!");
-} else {
-    alert("There is an error");
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("You've watch not many movies");
+    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        alert("You have good rate");
+    } else if (numberOfFilms > 30){
+        alert("You like movies!!!");
+    } else {
+        alert("There is an error");
+    }
 }
-console.log(personalMovieDB);
+//detectPersonalLevel();
+
+function writeYourGender() {
+    for (let i = 1; i <=3; i++) {
+        let movieGender = prompt(`What is your ${i} favorite genres?`);
+        personalMovieDB.genres[i - 1] = movieGender;
+    }
+}
+writeYourGender();
+
